70f5e7ad91633ae5785aed7ae9c7e1bc8ed9c16e
Chaudhary-sonika <111914130+Chaudhary-sonika@users.noreply.github.com>
2022-10-11 17:25:11 +0530

    feat: head image added

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var btnInterpret = document.querySelector("#interpret-btn");
+var inputPlace = document.querySelector("#input-place");
+var outputDiv = document.querySelector("#text-output");
+var errorDiv = document.querySelector("#error-div");
 
 var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
 
-function getTranslationURL(input) {
+function getApiURL(input) {
     return serverURL +"?"+"text="+ input
 }
 function errorHandler(error) {
     console.log("error occured: ", error);
     alert("Something wrong with the server! try again after some time")
 }
-function clickHandler() {
-    var inputText = txtInput.value;
-   fetch (getTranslationURL(inputText))
-  .then(response => response.json())
-  .then(json => {
-    var translatedText = json.contents.translated;
-    outputDiv.innerText = translatedText;
-})
-.catch(errorHandler)
+function clickHandler() { 
+    if(inputPlace.value !=" ") {
+        errorDiv.style.display = "none";
+        var inputText = inputPlace.value;
+        fetch (getApiURL(inputText))
+       .then(response => response.json())
+       .then(json => {
+         var interpretedText = json.contents.translated;
+         outputDiv.innerText = interpretedText;
+     })
+     .catch(errorHandler)
+    } else {
+        errorDiv.innerText = "Please enter sentence or a word";
+    }
+   
 };
-btnTranslate.addEventListener("click", clickHandler)
+btnInterpret.addEventListener("click", clickHandler)
