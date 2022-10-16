caa762da06b45b988f0eb28d9e121dc4f9ffeeea
Chaudhary-sonika <111914130+Chaudhary-sonika@users.noreply.github.com>
2022-10-16 22:32:11 +0530

    doc: updated

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var btnInterpret = document.querySelector("#interpret-btn");
+var btn = document.querySelector("#interpret-btn");
 var inputPlace = document.querySelector("#input-place");
-var outputDiv = document.querySelector("#text-output");
+var output = document.querySelector("#text-output");
 var errorDiv = document.querySelector("#error-div");
 
 var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
 
 function getApiURL(input) {
-    return serverURL +"?"+"text="+ input
+    return (serverURL +"?"+"text="+ input);
 }
-function errorHandler(error) {
-    console.log("error occured: ", error);
+function handlingError(error) {
+    console.log("error occured: ", + error);
     alert("Something wrong with the server! try again after some time")
 }
-function clickHandler() { 
+function btnClick() { 
     if(inputPlace.value !=" ") {
         errorDiv.style.display = "none";
         var inputText = inputPlace.value;
@@ -20,12 +20,12 @@ function clickHandler() {
        .then(response => response.json())
        .then(json => {
          var interpretedText = json.contents.translated;
-         outputDiv.innerText = interpretedText;
+         output.innerText = interpretedText;
      })
-     .catch(errorHandler)
+     .catch(handlingError);
     } else {
-        errorDiv.innerText = "Please enter sentence or a word";
+        errorDiv.innerText = "Please enter a sentence or word";
     }
    
-};
-btnInterpret.addEventListener("click", clickHandler)
+}
+btn.addEventListener("click", btnClick)
